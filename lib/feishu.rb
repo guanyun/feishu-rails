40e379f2345e35d1c2c22caa4a030cad7edc7768
ying.ma <cherry_822@126.com>
2023-08-08 15:00:18 +0800

    Update feishu.rb
    删除puts 代码

--- lib/feishu.rb
+++ lib/feishu.rb
@@ -22,24 +22,16 @@ module Feishu
   module_function
 
   def config
-    puts "feishu config ============================================ start"
-    puts "Thread.current['company'] => #{Thread.current['company']}, time => #{Time.now}"
     begin
       subco = Thread.current['company']
       feishu_config = Config.for(:feishu)
 
       selected_config = subco.blank? ?  feishu_config : feishu_config[subco]
-      puts "feishu selected_config ============================================ end", selected_config
-      puts "=================================================="
-
       OpenStruct.new(selected_config)
     end
   end
 
   def cipher
-    puts "feishu cipher ============================================ start"
-    puts "Thread.current['company'] => ", Thread.current['company'], "time => ", Time.now
-    puts "feishu cipher ============================================ end"
     require 'feishu/cipher'
     begin
       Cipher.new(config.encrypt_key)
